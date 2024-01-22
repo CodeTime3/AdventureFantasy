228b79cf98c73806e309ac16f6156959448a0813
CodeTime3 <daniele.pilloni.03@gmail.com>
2024-01-23 00:59:18 +0100

    Created Thief for the narration

---
+++ AdventureFantasy/Characters/Thief.cs
@@ -0,0 +1,21 @@
+﻿using AdventureFantasy.Abstractions;
+using AdventureFantasy.Interfaces.StoryInterfaces;
+
+namespace AdventureFantasy.Characters
+{
+    public class Thief
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public Thief(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Description}";
+        }
+    }
+}

f911e91f6b06def6eadfa4ba8171f5526f62db80
CodeTime3 <daniele.pilloni.03@gmail.com>
2024-01-23 01:16:25 +0100

    Created Amulet for the narration

---
+++ AdventureFantasy/Engine/Amulet.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventureFantasy.Engine
+{
+    public class Amulet
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+
+        public Amulet(string name, string description)
+        {
+            Name = name; // Heart of Aetheria
+            Description = description; //An ancient magical amulet with the power to control the elements
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Description}";
+        }
+    }
+}

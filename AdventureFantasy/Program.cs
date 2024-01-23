68fd86a29600a6b21183156ebc36b51ea7d999ff
CodeTime3 <daniele.pilloni.03@gmail.com>
2024-01-23 01:05:54 +0100

    Added some dependencies

--- AdventureFantasy/Program.cs
+++ AdventureFantasy/Program.cs
@@ -1,14 +1,26 @@
 ﻿using AdventureFantasy.Abstractions;
+using AdventureFantasy.Engine.DialogEngine;
+using AdventureFantasy.Engine.HeroEngine;
+using AdventureFantasy.Interfaces.HeroInterfaces;
+using AdventureFantasy.Interfaces.StoryInterfaces;
+using AdventureFantasy.Interfaces.StoryInterfaces.Adventure;
 
 namespace AdventureFantasy
 {
     internal class Program
     {
-        private static readonly ConsoleWrapper _console;
         static void Main(string[] args)
-        {
-            Game game = new Game(_console);
-            game.StartNewGame();
+        {   
+
+            //TODO: sistemare le dipendenze 
+
+            //IConsole console = new ConsoleWrapper();
+            //IPlayerNameProvider playerNameProvider = new CheckPlayerName(console);
+            //IPlayerRoleProvider playerRoleProvider = new ChooseHeroRole(console);
+            //IStoryTeller storyTeller = new GameStory(console);
+            //IAdventure adventure = new Dragon("Dragon", console);
+            //Game game = new Game(console, playerNameProvider, playerRoleProvider, storyTeller, adventure);
+            //game.StartNewGame();
         }
     }
 }
